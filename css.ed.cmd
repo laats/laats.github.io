1782b4a583adcd97fb0514f714ea646b52ec3e74
Staal A. Vinterbo <sav@ucsd.edu>
2015-03-09 15:08:37 -0700

    Initial version

---
+++ css.ed.cmd
@@ -0,0 +1,92 @@
+263a
+
+h1 a, h1 a:visited, h2 a, h2 a:visited, h3 a, h3 a:visited, h4 a, h4 a:visited, h5 a, h5 a:visited{
+    /* color:#20639b; */
+    color: inherit;
+    text-decoration:none;
+}
+
+h3.date {
+    font-size:70%;
+    text-align:right;
+}
+
+#TOC, #TOC a, #TOC a:visited {
+    text-decoration:none;    
+}
+
+img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+p.caption {
+    font-weight:bold;
+    font-size:90%;
+/*    margin-left: auto;
+    margin-right: auto; */
+    text-align: center;
+}
+
+#header {
+    border:1px solid #ddd;
+    background-color:#f1f1f1;
+    -webkit-border-radius:6px;
+    -moz-border-radius:6px;
+    border-radius:6px;
+    padding:10px;
+    margin-bottom: 20px;
+}
+
+/** */
+
+h1.title {
+    margin-top: 0;
+    margin-bottom:0;
+}
+
+
+h2.author {
+    border: none;
+    margin-bottom:0;
+    margin-top:0.25em;
+    font-size: 90%;
+}
+
+
+h3.date {
+    border: none;
+    margin: 0;
+}
+
+body {
+  /*  margin-right: 180px; */
+    margin-left: 25px;
+    margin-right: 25px;
+    margin-top: 25px;
+}
+.
+174c
+    font-size:95%;
+.
+168c
+/*    max-width:960px; */
+.
+95a
+li ol, li ul, li dl {
+    margin-top: 0;
+    margin-bottom: 0;
+}
+
+.
+91c
+{
+    margin:1em;
+}
+.
+7a
+
+
+
+.
